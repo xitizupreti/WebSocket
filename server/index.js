7a29b5a0c7a51e3a2206acbef547e16e521c4940
Kshitiz Upreti <upreti.kshitiz.ku@gmail.com>
2023-05-31 17:49:45 +0545

    server modules

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
-const io = new Server(3000,{
-    cors:{
-        origin:"*",
-    },
+const io = new Server(3000, {
+  cors: {
+    origin: ["http://localhost:3000","https://admin.socket.io"],
+    credentials: true,
+  },
 });
 
 io.on("connection", (socket) => {
   socket.emit("welcome", "welcome to RTS");
+  // sent a msg to the client
 
   socket.on("msg", (data) => {
-    console.log("msg from client", data);
+    console.log(data);
+    // recieve a msg from the client
   });
+});
+
+instrument(io, {
+  auth: false
 });
